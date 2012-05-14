d61c161ca372586de4c29e989f1fe093251f7fc5
the-edge <nicolas.ridel@viacesi.fr>
2012-05-14 22:58:40 +0200

    test

--- src/com/exia/android/ProjetandroidActivity.java
+++ src/com/exia/android/ProjetandroidActivity.java
@@ -9,5 +9,6 @@ public class ProjetandroidActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        test test
     }
 }
