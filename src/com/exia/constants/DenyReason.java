9a443849ed625465da1e0fe27e89e32dc01c4afb
Benoit <Benoit@Benoit-PC>
2012-05-18 12:11:33 +0200

    AJout des status

---
+++ src/com/exia/constants/DenyReason.java
@@ -0,0 +1,7 @@
+package com.exia.constants;
+
+public class DenyReason {
+	public static final int DOMAGED = 1;
+	public static final int DONT_LEAVE_AT_THIS_ADDRESS = 2;
+	public static final int UNDESIRABLE = 3;
+}
